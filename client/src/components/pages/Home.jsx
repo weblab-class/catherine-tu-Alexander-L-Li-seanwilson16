19d87b6a13720fdb095e8438b54579fcba03ff59
Catherine Tu <catherinetu@dhcp-10-31-166-139.dyn.MIT.EDU>
2025-01-15 16:40:00 -0500

    more home changes -- darker background, better spacing and centered

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -22,11 +22,13 @@ const Home = () => {
       ) : (
         <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
       )}
+      <PlayPauseHome />
       <div className="body" />
       <div className="homepage-container">
-        <h1 className="homepage-title">chilldeck</h1>
-        <p className="homepage-subtitle">made by chill people for chill people</p>
-        <PlayPauseHome />
+        <h1 className="homepage-title">
+          chilldeck
+          <p className="homepage-subtitle">made by chill people for chill people</p>
+        </h1>
         <div className="button-container">
           <button className="homepage-button">log in to dj</button>
           <button className="homepage-button">tutorial</button>
