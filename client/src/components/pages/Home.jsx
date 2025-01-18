d65836d2fe309db4cdb965b87872ae1c93f95cb9
Catherine Tu <catherinetu@dhcp-10-31-166-139.dyn.mit.edu>
2025-01-17 23:33:22 -0500

    TEST with lofi img

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -2,6 +2,7 @@ import "../../utilities.css";
 import "./Home.css";
 import PlayPauseHome from "../modules/PlayPauseHome";
 import HomeButtons from "../modules/HomeButtons";
+import lofi from "../../public/assets/lofi-background-purple-blue.jpg";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <div className="homepage-container">
         <h1 className="homepage-title">
           chilldeck
+          <img src={lofi} />
           <p className="homepage-subtitle">made by chill people for chill people</p>
         </h1>
         <HomeButtons />
