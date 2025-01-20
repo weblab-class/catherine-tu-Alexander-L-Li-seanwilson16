cdf51feb08cefac02feb2833076e0a937a6f8e4c
Catherine Tu <catherinetu@dhcp-10-31-166-139.dyn.MIT.EDU>
2025-01-20 17:41:07 -0500

    404 not found please hopefully no one shows this page

--- client/src/components/pages/NotFound.jsx
+++ client/src/components/pages/NotFound.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import "./NotFound.css";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 Not Found</h1>
-      <p>The page you requested couldn't be found.</p>
+    <div className="body">
+      <h1 className="not-found-text">404 Not Found</h1>
+      <p className="subtitle-text">
+        The page you requested couldn't be found. It's okay, it's chill.
+      </p>
+      <img src={"favicon.png"} alt="favicon..." className="not-found-img" />
     </div>
   );
 };
