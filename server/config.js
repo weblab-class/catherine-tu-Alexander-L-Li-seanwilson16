979b489164fe1e1395bd544033f2e56db149330c
Sean Wilson <sjwilson@mit.edu>
2025-01-29 07:46:02 -0500

    Revert "Merge branch 'main' of https://github.com/weblab-class/catherine-tu-Alexander-L-Li-seanwilson16"
    This reverts commit 7a3e8a39354cddfb0133e40e2d56fdbf993c5537, reversing
    changes made to 6d0b5d026481ac9f635412c671b00126fc1b0cd6.

--- server/config.js
+++
@@ -1,8 +0,0 @@
-// Paths for file storage
-const isProduction = process.env.NODE_ENV === "production";
-const BASE_PATH = isProduction ? "/data" : ".";
-
-module.exports = {
-  UPLOADS_DIR: `${BASE_PATH}/uploads`,
-  STEMS_DIR: `${BASE_PATH}/stems`,
-};
