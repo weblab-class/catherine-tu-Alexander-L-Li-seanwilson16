96a5fbec516a7179cb6c450f9b610a5437932b31
Alexander Li <alxli@Alexanders-MacBook-Pro.local>
2025-01-29 01:34:20 -0500

    import dropdown

--- client/src/components/pages/DJ.jsx
+++ client/src/components/pages/DJ.jsx
@@ -218,13 +218,6 @@ const DJ = () => {
 
   const [syncEnabled, setSyncEnabled] = useState(false);
 
-  const formatTime = (seconds) => {
-    if (isNaN(seconds)) return "0:00";
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = Math.floor(seconds % 60);
-    return `${minutes}:${remainingSeconds.toString().padStart(2, "0")}`;
-  };
-
   const leftContainerRef = useRef(null);
   const rightContainerRef = useRef(null);
   const leftWaveformRef = useRef(null);
