373c2b1538cb894cf2135c76d4787101e4416891
Sean Wilson <sjwilson@mit.edu>
2025-01-29 04:18:11 -0500

    added keybinds back to help page

--- server/server.js
+++ server/server.js
@@ -70,7 +70,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
-      secure: false, // set to true if using https
+      secure: true, // set to true if using https
     }
   })
 );
